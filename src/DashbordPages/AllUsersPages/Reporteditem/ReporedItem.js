ba0a06d15baf30524dd225b3942bc1beb947c6b8
Tarek <mdtareksolaiman16@gmail.com>
2022-11-26 16:26:13 +0600

    reported item delet function is ready

--- src/DashbordPages/AllUsersPages/Reporteditem/ReporedItem.js
+++ src/DashbordPages/AllUsersPages/Reporteditem/ReporedItem.js
@@ -1,9 +1,53 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import Loading from "../../../SharedPage/Loading";
+import ReportCard from "./ReportCard";
 
 const ReporedItem = () => {
+  const {
+    data: reportData = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: "reportData",
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/admin/report", {
+        headers: {
+          authorization: `bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+      const data = await res.json();
+      return data;
+    },
+  });
+  console.log(reportData);
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <div>
-      <h1 className="text-3xl font-semibold">This Reported item page</h1>
+      <h1 className="text-3xl font-semibold">All Reported item</h1>
+      <div className="overflow-x-auto w-full my-20">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>product Name</th>
+              <th>seller EmailID</th>
+              <th>price</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {reportData?.map((report) => (
+              <ReportCard
+                key={report._id}
+                report={report}
+                refetch={refetch}
+              ></ReportCard>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
---
+++ src/DashbordPages/AllUsersPages/Reporteditem/ReportCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { toast } from "react-toastify";
+
+const ReportCard = ({ report, refetch }) => {
+  const { productName, email, resalePrice, _id } = report;
+  const deleteItem = (id) => {
+    fetch(`http://localhost:5000/admin/report/${id}`, {
+      method: "DELETE",
+      headers: {
+        authorization: `bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          toast.success("Success full delete", {
+            autoClose: 500,
+          });
+          refetch();
+        } else {
+          toast.error(data.message, {
+            autoClose: 500,
+          });
+        }
+      });
+  };
+  return (
+    <tr>
+      <td>{productName}</td>
+      <td>{email}</td>
+      <td>{resalePrice}</td>
+      <th>
+        <button
+          onClick={() => deleteItem(_id)}
+          className="btn btn-error btn-xs"
+        >
+          delete
+        </button>
+      </th>
+    </tr>
+  );
+};
+
+export default ReportCard;
